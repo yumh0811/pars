d639637f6d55262f8448a979da0142cab941ea06
yumh0811 <yumh0811@gmail.com>
2018-05-06 14:35:01 +0800

    Update Scer_n157_nonMosaic_Spar_distance_processed.R

--- program/Scer_n157_nonMosaic_Spar_distance_processed.R
+++ program/Scer_n157_nonMosaic_Spar_distance_processed.R
@@ -2,21 +2,15 @@ setwd("~/data/mrna-structure/phylogeny/")
 library(plyr)
 
 data_gene_range = read.csv("~/data/mrna-structure/phylogeny/Scer_n157_nonMosaic_Spar_gene_range.csv",head=TRUE)
-data_mean_distance = read.csv("~/data/mrna-structure/phylogeny/Scer_n157_nonMosaic_Spar_mean_distance.csv",head=TRUE)
-
-data_mean_distance = merge(data_gene_range,data_mean_distance,by="gene")
-
 data_cds_range_chr = read.table("~/data/mrna-structure/phylogeny/protein_coding_list_range_chr.csv",head=FALSE,sep = ",")
 colnames(data_cds_range_chr) = c("gene","chr","start","end")
-
-data_out = merge(data_cds_range_chr,data_mean_distance,by="gene")
-data_out <- arrange(data_out,data_out$chr,data_out$start)
-write.csv(data_out,file="~/data/mrna-structure/phylogeny/Scer_n157_nonMosaic_Spar_distance_processed.csv",row.names = FALSE)
-
-# require full alignment (proporation = 1)
+data_gene_range = merge(data_cds_range_chr,data_gene_range,by="gene")
+data_out <- arrange(data_gene_range,data_gene_range$chr,data_gene_range$start)
 data_out_1 = subset(data_out, data_out$proporation==1)
 write.csv(data_out_1,file="~/data/mrna-structure/phylogeny/Scer_n157_nonMosaic_Spar_distance_processed_pro_1.csv",row.names = FALSE)
 
+data_mean_distance = read.csv("~/data/mrna-structure/phylogeny/Scer_n157_nonMosaic_Spar_mean_distance.csv",head=TRUE)
+data_out_1 = merge(data_out_1,data_mean_distance,by="gene")
 data_out_1 = subset(data_out_1, select = c(gene,chr,start,end,alignment_cds_length,sgd_cds_length,intersection_length,proporation,asian,wine,beer1,beer2,mixed))
 
 data_gene_nodom <- data.frame()
