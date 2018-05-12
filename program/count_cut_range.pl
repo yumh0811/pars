240ef790e5309c28a7bb83b0d16709f0f3f4f896
yumh0811 <yumh0811@gmail.com>
2018-05-12 14:38:37 +0800

    Add files via upload

---
+++ program/count_cut_range.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+
+use File::Find::Rule;
+use AlignDB::IntSpan;
+use YAML::Syck qw();
+
+use Getopt::Long qw();
+use FindBin;
+
+#----------------------------------------------------------#
+# GetOpt section
+#----------------------------------------------------------#
+
+=head1 SYNOPSIS
+
+    perl ~/Scripts/pars/program/count_cut_range.pl --file ~/data/mrna-structure/process/Scer_n157_nonMosaic_Spar.gene_variation.process.yml --cut ~/data/mrna-structure/process/sce_cds.yml --output stem_loop_cds_length.csv 
+    perl ~/Scripts/pars/program/count_cut_range.pl --file ~/data/mrna-structure/process/Scer_n157_nonMosaic_Spar.gene_variation.process.yml --cut ~/data/mrna-structure/process/sce_utr.yml --output stem_loop_utr_length.csv
+
+=cut
+
+Getopt::Long::GetOptions(
+    'help|?'     => sub { Getopt::Long::HelpMessage(0) },
+    'file|f=s'   => \my $file,
+    'cut|c=s'   => \my $cut,
+    'output|o=s' => \my $output,
+) or Getopt::Long::HelpMessage(1);
+
+  
+open OUT, ">",$output;
+print OUT "gene" . ",". "chr" . ",". "stem_length" .",". "loop_length"."\n";
+
+print "Load $file\n";
+my $gene_info_of = YAML::Syck::LoadFile($file);
+
+foreach my $gene ( sort keys %{$gene_info_of} ){
+	print $gene."\n";
+	my $info = $gene_info_of->{ $gene };
+  my $stem = AlignDB::IntSpan->new;
+  $stem->add( $info->{fold_left} );
+  $stem->add( $info->{fold_right} );
+  #print $stem."\n"."\n";sleep 1;
+  my $loop = AlignDB::IntSpan -> new($info->{fold_dot});
+  #print $loop."\n";sleep 1;
+  
+  my $chr = $info->{chr};
+  my $chr_set = AlignDB::IntSpan->new($info->{chr_set});
+  my @chr_set = $chr_set->ranges();
+  #print $chr_set[0]."\n";
+
+  #transfer relative position to absolute position
+  my $stem2 = $stem->map_set(sub{$_+$chr_set[0]-1});
+  #print $stem2."\n"."\n";sleep 1;
+  my $loop2 = $loop->map_set(sub{$_+$chr_set[0]-1});
+  #print $loop2."\n"."\n";sleep 1;
+  
+  print "Load $cut\n";
+  my $gene_info2_of = YAML::Syck::LoadFile($cut);
+  
+  my $info2 = $gene_info2_of->{ $chr };
+
+  
+  my $cut_range = AlignDB::IntSpan->new;
+  $cut_range->add( $info2 );
+  #print $cut_range."\n";sleep 1;
+  
+  my $stem_intersection = AlignDB::IntSpan::intersect( $stem2, $cut_range );
+  print $stem_intersection."\n";
+  my $stem_intersection_length = $stem_intersection->cardinality();           
+  my $loop_intersection = AlignDB::IntSpan::intersect( $loop2, $cut_range );
+  print $loop_intersection."\n";
+  my $loop_intersection_length = $loop_intersection->cardinality();             
+                
+  open OUT,">>",$output;
+  print OUT "$gene" . ",". "$chr" . ",". "$stem_intersection_length" . ",". "$loop_intersection_length" . "\n";     
+}	
+	
+
+
+close OUT;
+__END__
---
+++ program/count_per_gene_ACGT_percent.pl
@@ -0,0 +1,209 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+
+use Getopt::Long qw();
+use FindBin;
+use YAML::Syck qw();
+
+use Path::Tiny;
+
+use AlignDB::IntSpan;
+use AlignDB::Stopwatch;
+use App::Rangeops;
+
+use Statistics::ChisqIndep;
+use POSIX;
+use Data::Dumper "Dumper";
+
+#----------------------------------------------------------#
+# GetOpt section
+#----------------------------------------------------------#
+
+=head1 SYNOPSIS
+
+    perl ~/Scripts/pars/program/count_per_gene_ACGT_percent.pl --file data_SNPs_PARS_nsy.csv --output data_SNPs_PARS_nsy_per_gene_ATGC.csv
+
+=cut
+
+Getopt::Long::GetOptions(
+    'help|?'     => sub { Getopt::Long::HelpMessage(0) },
+    'file|f=s'   => \my $file,
+    'output|o=s' => \my $output,
+) or Getopt::Long::HelpMessage(1);
+
+my $csv_fh;
+open $csv_fh, '<', $file;
+
+open OUT, '>', $output;
+
+my %mutation_AT_GC =
+  ( 'A->C' => 0, 'A->G' => 0, 'T->C' => 0, 'T->G' => 0 );    #A/T->G/C
+my %mutation_GC_AT =
+  ( 'G->A' => 0, 'G->T' => 0, 'C->A' => 0, 'C->T' => 0 );    #G/C->A/T
+my %mutation_other =
+  ( 'G->C' => 0, 'C->G' => 0, 'T->A' => 0, 'A->T' => 0 );    #G<->C,A<->T
+my %gene_name;
+my %count_SNPs = (
+    'stem_AT_GC' => 0,
+    'stem_GC_AT' => 0,
+    'loop_AT_GC' => 0,
+    'loop_GC_AT' => 0
+);
+
+while (<$csv_fh>) {
+    chomp;
+    s/\"//g;
+    my @content = split /,/, $_;
+    my @content_new;
+    if ( $content[0] eq "gene" ) {
+        $content_new[0] = "gene";
+        $content_new[1] = "stem_AT_GC";
+        $content_new[2] = "stem_GC_AT";
+        $content_new[3] = "stem_total";
+        $content_new[4] = "loop_AT_GC";
+        $content_new[5] = "loop_GC_AT";
+        $content_new[6] = "loop_total";
+        #$content_new[7] = "stem_AT_GC_ratio";
+        #$content_new[8] = "loop_AT_GC_ratio";
+        #$content_new[9] = "X2";
+        #$content_new[10] = "p_value";
+        my $content_new = join ",", @content_new;
+        open OUT,'>>',$output;
+        print OUT $content_new, "\n";
+
+    }
+    else {
+
+        if ( exists $gene_name{ $content[0] } ) {
+
+            if ( $content[7] eq "stem" ) {
+                if ( exists $mutation_AT_GC{ $content[10] } ) {
+                    $count_SNPs{'stem_AT_GC'} += 1;
+                }
+                elsif ( exists $mutation_GC_AT{ $content[10] } ) {
+                    $count_SNPs{'stem_GC_AT'} += 1;
+                }elsif ( exists $mutation_other{ $content[10] } ) {
+                	  $count_SNPs{'stem_other'} += 1;
+                }
+            }
+            else {
+                if ( exists $mutation_AT_GC{ $content[10] } ) {
+                    $count_SNPs{'loop_AT_GC'} += 1;
+                }
+                elsif ( exists $mutation_GC_AT{ $content[10] } ) {
+                    $count_SNPs{'loop_GC_AT'} += 1;
+                }elsif ( exists $mutation_other{ $content[10] } ) {
+                	  $count_SNPs{'loop_other'} += 1;
+                }
+            }
+
+            #my %hash = %count_SNPs;
+            $gene_name{ $content[0] } = {%count_SNPs};
+
+            #print $content[0],"\n";
+            #print Dumper(\%gene_name);sleep 2;
+        }
+        else {
+
+            %count_SNPs = (
+                'stem_AT_GC' => 0,
+                'stem_GC_AT' => 0,
+                'loop_AT_GC' => 0,
+                'loop_GC_AT' => 0,
+                'stem_other' => 0,
+                'loop_other' => 0
+            );
+
+            if ( $content[7] eq "stem" ) {
+                if ( exists $mutation_AT_GC{ $content[10] } ) {
+                    $count_SNPs{'stem_AT_GC'} += 1;
+                }
+                elsif ( exists $mutation_GC_AT{ $content[10] } ) {
+                    $count_SNPs{'stem_GC_AT'} += 1;
+                }elsif ( exists $mutation_other{ $content[10] } ) {
+                	  $count_SNPs{'stem_other'} += 1;
+                }
+            }
+            elsif ( $content[7] eq "loop" ) {
+                if ( exists $mutation_AT_GC{ $content[10] } ) {
+                    $count_SNPs{'loop_AT_GC'} += 1;
+                }
+                elsif ( exists $mutation_GC_AT{ $content[10] } ) {
+                    $count_SNPs{'loop_GC_AT'} += 1;
+                }elsif ( exists $mutation_other{ $content[10] } ) {
+                	  $count_SNPs{'loop_other'} += 1;
+                }
+            }
+
+            #my %hash = %count_SNPs;
+            $gene_name{ $content[0] } = {%count_SNPs};
+
+            #print $content[0],"\n";
+            #print Dumper(\%gene_name);sleep 2;
+
+        }
+    }
+
+}
+
+#print "=============";
+#print  scalar(keys %gene_name);
+
+foreach my $keys ( sort keys %gene_name ) {
+    my @content_new;
+    $content_new[0] = $keys;
+
+    my $stem_AT_GC = $gene_name{$keys}{'stem_AT_GC'};
+    my $stem_GC_AT = $gene_name{$keys}{'stem_GC_AT'};
+    my $loop_AT_GC = $gene_name{$keys}{'loop_AT_GC'};
+    my $loop_GC_AT = $gene_name{$keys}{'loop_GC_AT'};
+    my $stem_other = $gene_name{$keys}{'stem_other'};
+    my $loop_other = $gene_name{$keys}{'loop_other'};
+    my $stem_total = $stem_AT_GC + $stem_GC_AT + $stem_other;
+    my $loop_total = $loop_AT_GC + $loop_GC_AT + $loop_other;
+    my $stem_AT_GC_ratio;
+    my $loop_AT_GC_ratio;
+
+#    if ( $stem_AT_GC + $stem_GC_AT + $stem_other!= 0 ) {
+#        $stem_AT_GC_ratio = $stem_AT_GC / ( $stem_AT_GC + $stem_GC_AT + $stem_other );
+#    }
+#    if ( $loop_AT_GC + $loop_GC_AT + $loop_other!= 0 ) {
+#        $loop_AT_GC_ratio = $loop_AT_GC / ( $loop_AT_GC + $loop_GC_AT + $loop_other);
+#    }
+    
+    if ( $stem_AT_GC + $loop_AT_GC != 0 ) {
+        $stem_AT_GC_ratio = $stem_AT_GC / ( $stem_AT_GC + $loop_AT_GC );
+    }
+    if ( $stem_AT_GC + $loop_AT_GC != 0 ) {
+        $loop_AT_GC_ratio = $loop_AT_GC / ( $stem_AT_GC + $loop_AT_GC );
+    }
+
+    my $obs = [ [ $stem_AT_GC, $stem_GC_AT ], [ $loop_AT_GC, $loop_GC_AT ] ];
+    my $chi = new Statistics::ChisqIndep;
+    $chi->load_data($obs);
+    $chi->print_summary();
+    my $chisq = ${$chi}{'chisq_statistic'};
+    my $P     = ${$chi}{'p_value'};
+
+    $content_new[1] = $stem_AT_GC;
+    $content_new[2] = $stem_GC_AT;
+    $content_new[3] = $stem_total;
+    $content_new[4] = $loop_AT_GC;
+    $content_new[5] = $loop_GC_AT;
+    $content_new[6] = $loop_total;
+    #$content_new[7] = $stem_AT_GC_ratio;
+    #$content_new[8] = $loop_AT_GC_ratio;
+    #$content_new[9] = $chisq;
+    #$content_new[10] = $P;
+
+    my $content_new = join ",", @content_new;
+    open OUT,'>>',$output;;
+    print OUT $content_new, "\n";
+}
+
+close $csv_fh;
+close OUT;
+
+__END__
