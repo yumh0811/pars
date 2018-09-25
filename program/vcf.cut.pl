c2ada59475f95661d1cd738ebd08e6e44b2130d2
yumh0811 <yumh0811@gmail.com>
2018-09-25 11:33:51 +0800

    Add vcf program

---
+++ program/vcf.cut.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use autodie;
+
+use Getopt::Long qw();
+use FindBin;
+use YAML::Syck qw();
+
+use Path::Tiny;
+
+use AlignDB::IntSpan;
+use AlignDB::Stopwatch;
+use App::Rangeops;
+
+use POSIX;
+use Data::Dumper "Dumper";
+
+#----------------------------------------------------------#
+# GetOpt section
+#----------------------------------------------------------#
+
+=head1 SYNOPSIS
+    
+    cd /Volumes/Backup/yumh/data/vcf/1011Matrix.gvcf/
+    perl cut.pl --file chr1.gvcf --output chr1.tsv
+
+=cut
+
+Getopt::Long::GetOptions(
+    'help|?'     => sub { Getopt::Long::HelpMessage(0) },
+    'file|f=s'   => \my $file,
+    'output|o=s' => \my $output,
+) or Getopt::Long::HelpMessage(1);
+
+my $tsv_fh;
+open $tsv_fh, '<', $file;
+
+open OUT, '>', $output;
+
+while (<$tsv_fh>) {
+    chomp;
+    if (m/^\#\#/) {
+        next;
+    }else{
+        my @content = split /\t/, $_;
+        splice @content, 8;
+        my $content = join "\t", @content;
+        open OUT,'>>',$output;;
+        print OUT $content, "\n";
+    }
+}
+
+close $tsv_fh;
+close OUT;
+
+__END__
