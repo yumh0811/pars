d514b566a33f6fe59bb88121f918cd3e52da5f1c
yumh0811 <yumh0811@gmail.com>
2018-09-25 21:45:02 +0800

    Delete subset.R

--- program/vcf.merge.subset.R
+++
@@ -1,7 +0,0 @@
-setwd("/Volumes/Backup/yumh/data/vcf/1011Matrix.gvcf/")
-
-chr1_pars <- read.csv("chr1.pars.tsv", head= TRUE, sep = '\t')
-chr1_ext <- read.csv("chr1.subset.ext.tsv", head= TRUE, sep = '\t')
-
-chr1_merge <- merge(chr1_pars, chr1_ext, by = "name", all = F)
-write.table (chr1_merge, file = "chr1.merge.subset.tsv", row.names = F,sep = "\t")
