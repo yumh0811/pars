50e804651899ff5f4810edb85c1e6ced93ec4efd
yumh0811 <yumh0811@gmail.com>
2018-09-26 22:59:34 +0800

    Delete bash

--- program/count_structure_length_gene.sh
+++
@@ -1,5 +0,0 @@
-export NAME=Scer_n157_nonMosaic_Spar
-cd ~/data/mrna-structure/result/$NAME
-perl ~/Scripts/pars/program/count_structure_length_gene.pl --file ~/data/mrna-structure/process/$NAME.gene_variation.process.yml --name ~/data/mrna-structure/result/Scer_n157_nonMosaic_Spar/PARS_cds_gene.csv --structure stem --output stem_length_cds.csv
-perl ~/Scripts/pars/program/count_structure_length_gene.pl --file ~/data/mrna-structure/process/$NAME.gene_variation.process.yml --name ~/data/mrna-structure/result/Scer_n157_nonMosaic_Spar/PARS_cds_gene.csv --structure loop --output loop_length_cds.csv
-unset NAME
